028a0984e65c4474161f8c384e7e38160a686ccf
ATHARVISM2804 <atharv.golait@gmail.com>
2026-03-11 18:16:29 +0530

    update

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -13,15 +13,21 @@ import {
 } from 'react-icons/hi2';
 
 const socialLinks = [
-  { label: 'LinkedIn', href: '#', icon: (
-    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
-  )},
-  { label: 'Facebook', href: '#', icon: (
-    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/></svg>
-  )},
-  { label: 'Twitter', href: '#', icon: (
-    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
-  )},
+  {
+    label: 'LinkedIn', href: '#', icon: (
+      <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+    )
+  },
+  {
+    label: 'Facebook', href: '#', icon: (
+      <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" /></svg>
+    )
+  },
+  {
+    label: 'Twitter', href: '#', icon: (
+      <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" /></svg>
+    )
+  },
 ];
 
 const trustBadges = [
@@ -253,7 +259,7 @@ export default function Footer() {
           </p>
           <div className="flex items-center gap-6">
             <div className="flex gap-6 text-xs text-gray-500">
-              <Link to="/privacy-policy" className="hover:text-white transition-colors duration-300 relative group/legal">
+              <Link to="/privacypolicy" className="hover:text-white transition-colors duration-300 relative group/legal">
                 Privacy Policy
                 <span className="absolute -bottom-0.5 left-0 w-0 group-hover/legal:w-full h-[1px] bg-accent/50 transition-all duration-300 rounded-full" />
               </Link>
@@ -261,7 +267,7 @@ export default function Footer() {
                 Terms of Service
                 <span className="absolute -bottom-0.5 left-0 w-0 group-hover/legal:w-full h-[1px] bg-accent/50 transition-all duration-300 rounded-full" />
               </a>
-             
+
             </div>
             {/* Back to top */}
             <motion.button
